9eda4c3cdc1a286c7bdd8547d05b620d500443eb
Ekultek <ekultek@noreply.github.com>
2022-11-27 22:35:28 -0600

    initial push

---
+++ README.md
@@ -0,0 +1,47 @@
+# Malcore Dynamic Emulation Ghidra Plugin
+
+This plugin allows you to run dynamic emulation using the Malcore API. It provides an emulation of the program run in a Windows environment and allows you to understand how the program runs dynamically without the need for a sandbox.
+
+![Run](https://user-images.githubusercontent.com/14183473/204192515-fbfd29c2-d53b-43f7-94a9-f27f959c9d72.gif)
+
+### Dependencies
+
+#### Libraries
+
+This plugin requires the requests library version 2.27.1. You can install this library by running:
+
+```
+pip2 install --user requests==2.27.1
+```
+
+If you do not have `pip` installed for Python 2.x you can get pip from the following script: https://bootstrap.pypa.io/pip/2.7/get-pip.py
+
+#### API Key
+
+In order to run this plugin you need a Malcore API key, you can get one from https://malcore.io. The API key will need to be set inside an environment variable called `MALCORE_API_KEY`
+
+### Installation
+
+This plugin was tested from Ghidra version 9.2.3 - 10.1.4
+
+To install the plugin you will need to do the following:
+
+1. Clone the repository into a path of your desire
+2. From inside the CodeBrowser click Window > Bundle Manager as seen below:
+
+![installation_step_1](https://user-images.githubusercontent.com/14183473/204193086-689c6c0a-dbbe-42f6-abe4-908e8f5daa0d.jpg)
+
+3. From inside the bundle manager click the green `+` and navigate to the location you cloned this repository to as seen below:
+
+![installation_step_2](https://user-images.githubusercontent.com/14183473/204194399-7fe159b5-9f41-41df-bbc8-3ce7134e269a.jpg)
+
+4. Make sure that your path is checked and close the bundle manager window
+5. Now go to the script manager
+
+![installation_step_3](https://user-images.githubusercontent.com/14183473/204193660-b2f76114-ecda-4ef8-9b5b-5dc3f3090c72.jpg)
+
+6. Search for Malcore and check the "In Tool" box to activate the toolbar button and the Shift-M keybinding
+
+![installation_step_4](https://user-images.githubusercontent.com/14183473/204193855-68bd016b-dbec-4f59-8c4a-752b50b0302d.jpg)
+
+After this the plugin should be installed and able to be run.
---
+++ malcore_dynamic_emulation.py
@@ -0,0 +1,266 @@
+# Ghidra plugin for Malcore API to run dynamic emulation on files
+# @author Internet 2.0, Ltd
+# @category Analysis
+# @keybinding shift M
+# @toolbar malcore.png
+
+import json
+import os
+import sys
+import time
+import tempfile
+
+
+PLATFORM = ""
+
+
+# fixes the Ghidra variables and appends the working OS Python path to them
+if (os.name == "Posix" or os.name.getshadow() == "posix") and (("Linux") in os.uname()):
+    PLATFORM = "linux"
+    sys.path.append('/usr/lib/python2.7/dist-packages')
+    sys.path.append('/usr/local/lib/python2.7/dist-packages')
+    sys.path.append(os.path.expanduser('~') + '/.local/lib/python2.7/site-packages')
+elif "Darwin" in os.uname():
+    PLATFORM = "macos"
+    sys.path.append('/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
+    sys.path.append('/System/Library/Frameworks/Python.framework/Versions/2.7/lib/site-python')
+    sys.path.append('/Library/Python/2.7/site-packages')
+    sys.path.append(os.path.expanduser('~') + '/Library/Python/2.7/lib/python/site-packages')
+elif os.name == "nt" or "windows" in java.lang.System.getProperty("os.name").lower():
+    PLATFORM = "windows"
+    sys.path.append('C:\\Python27\\lib\\site-packages')
+elif os.name == "java":
+    PLATFORM = "other"
+    sys.path.append('/usr/lib/python2.7/dist-packages')
+    sys.path.append('/usr/local/lib/python2.7/dist-packages')
+    sys.path.append('/usr/local/lib/python2.7/site-packages')
+else:
+    print("failed to add correct PATH, unable to detect platform")
+    exit(1)
+
+
+try:
+    import requests
+except ImportError:
+    print("you need to install the requests module version 2.27.1: `pip install --user requests==2.27.1`")
+    exit(1)
+
+
+class Formatter(object):
+
+    """
+    class to format the output from the dynamic analysis results.
+    """
+
+    total_suspicious_calls = 0
+
+    def __init__(self, dynamic_results):
+        self.dynamic_results = dynamic_results
+        self.emulation_results = []
+
+    def build_map(self):
+        """
+        builds a list containing the information we'll need to output
+        """
+        log("building output map from emulation results", "debug")
+        for data in self.dynamic_results["parsed_output"]:
+            self.emulation_results.append([
+                data['dll_name'],
+                data['function_called'],
+                data['arguments_passed'],
+                data['function_return_value'],
+                data['known_suspicious_function'],
+                data['location']
+            ])
+
+    def build_output_table(self):
+        """
+        formats the list that was initialized above
+        """
+        log("attempting to build output from emulation", "debug")
+        split_string = "*" * 85
+        print("\n\nDYNAMIC EMULATION REPORT:")
+        print("{}\n".format(split_string))
+        for row in self.emulation_results:
+            if row[4] != 0:
+                self.total_suspicious_calls += 1
+            output_string = "({})>>> {} --> {}.{}".format(row[4], row[-1], row[0], row[1])
+            if len(row[2]) != 0:
+                output_string += "("
+                for i, arg in enumerate(row[2], start=1):
+                    if i != len(row[2]):
+                        output_string += '{},'.format(arg)
+                    else:
+                        output_string += '{}'.format(arg)
+                output_string += ")"
+            else:
+                output_string += "()"
+            output_string += " => {}".format(row[3])
+            print(output_string)
+        print("\n{}\nFormat: (SUSPICION LEVEL)>>> ADDRESS --> DLL-NAME.FUNCTION-NAME(ARGS) => RETURN-VALUE\n\n".format(
+            split_string
+        ))
+
+
+class ApiHandler(object):
+
+    def __init__(self, base_url, api_key):
+        self.base_url = base_url
+        self.api_key = api_key
+
+    def dynamic_emulation(self, file_path):
+        """
+        responsible for the request to the API for analysis
+        """
+        headers = {"apiKey": self.api_key}
+        emulate_url = "{}/dynamicanalysis".format(self.base_url)
+        data = {"filename1": open(file_path, "rb")}
+        try:
+            req = requests.post(emulate_url, headers=headers, files=data)
+            results = req.json()
+        except:
+            results = {}
+        temp = tempfile.NamedTemporaryFile(delete=False, prefix="malcore_results_", suffix=".json")
+        if len(results) != 0:
+            json.dump(results, temp, indent=4)
+            log("full dynamic emulation results saved to: {}".format(temp.name), "info")
+        else:
+            log("error during analysis will not save empty file", "info")
+        temp.close()
+        return results
+
+
+TOTAL_SUSPICIOUS_CALLS = 0
+VERSION = "0.1"
+VERSION_STRING = "dev" if VERSION.count(".") > 3 else "stable"
+URL = "https://malcore.io"
+LOGO = """    __  ___      __                   
+   /  |/  /___ _/ /________  ________ 
+  / /|_/ / __ `/ / ___/ __ \/ ___/ _ \\
+ / /  / / /_/ / / /__/ /_/ / /  /  __/
+/_/  /_/\\__,_/_/\\___/\\____/_/   \\___/ 
+Malware analysis made simple. v{}({})
+\t{}
+\tInternet 2.0, Ltd""".format(VERSION, VERSION_STRING, URL)
+
+
+class MalcoreDynamicEmulationPlugin(object):
+
+    base_url = "http://127.0.0.1:9080"
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def run(self):
+        """
+        this function is responsible for running the analysis and performing all other
+        operations of the plugin
+        """
+        path = currentProgram.getExecutablePath()
+        if PLATFORM == "windows":
+            # fix the path so we don't get a leading "/" in it on Windows
+            path = path[1:]
+        file_size = os.stat(path)
+        # 25MB is the max file size this plugin can handle
+        if file_size.st_size > 26214400:
+            log("only able to handle up to 25mb files", "error")
+            return
+        if not is_pe(path):
+            log("can only handle Windows PE files for emulation", "error")
+            return
+        program_name = currentProgram.getName()
+        creation_date = currentProgram.getCreationDate()
+        language_id = currentProgram.getLanguageID()
+        compiler_spec_id = currentProgram.getCompilerSpec().getCompilerSpecID()
+        log("current file path: {}".format(path), level="debug")
+        if not path:
+            log("file path does not exist".format(path), level="error")
+            return
+        log("current program name: {}".format(program_name), level="debug")
+        log("database creation date: {}".format(creation_date), level="debug")
+        log("file language ID: {}".format(language_id), level="debug")
+        log("compiler information: {}".format(compiler_spec_id), level="debug")
+        log("sending file: {} to start emulation".format(path), level="INFO")
+        emulator = ApiHandler(self.base_url, self.api_key)
+        results = emulator.dynamic_emulation(path)
+        if len(results) == 0:
+            log("dynamic emulation was not successful", "error")
+        return results
+
+
+def log(log_string, level="info"):
+    """
+    logs information to the screen:
+    """
+    print("[{}][{}] {}".format(
+        time.strftime("%H-%M-%S"), level.upper(), log_string
+    ))
+
+
+def get_api_key():
+    """
+    gathers the API key from the associated environment variable
+    """
+    env = os.environ
+    for key in env.keys():
+        if "MALCORE_API_KEY" in key:
+            return env[key]
+    log("MALCORE_API_KEY environment variable is not set with associated API key", "error")
+    return None
+
+
+def is_pe(filename):
+    """
+    simple check to determine if a file is a Windows PE file or not
+    """
+    with open(filename, "rb") as fh:
+        if fh.read(2) == b"MZ":
+            return True
+    return False
+
+
+def post_analysis_report(dynamic_results, formatter_class_obj):
+    """
+    creates a post analysis report that provides information after runtime
+    """
+    data = dynamic_results["dynamic_analysis"][0]
+    total_emulation_runtime = data["emulation_total_runtime"]
+    architecture_run_as = data['arch']
+    os_run = data["os_run"]
+    api_hash_calculated = data['entry_points'][0]['apihash']
+    print(
+        "POST ANALYSIS REPORT:\n"
+        "{}\nTotal runtime: {}\n"
+        "Architecture: {}\n"
+        "OS run: {}\n"
+        "Total suspicious calls: {}\nAPI hash: {}\n".format(
+            "*" * 21,
+            total_emulation_runtime,
+            architecture_run_as,
+            os_run,
+            formatter_class_obj.total_suspicious_calls,
+            api_hash_calculated
+        )
+    )
+
+
+def main():
+    """
+    main function
+    """
+    print("\n{}\n\n".format(LOGO))
+    api_key = get_api_key()
+    if api_key is None:
+        exit(1)
+    results = MalcoreDynamicEmulationPlugin(api_key).run()
+    if not len(results) == 0:
+        format_ = Formatter(results)
+        format_.build_map()
+        format_.build_output_table()
+        post_analysis_report(results, format_)
+    log("emulation performed successfully", "info")
+
+
+if __name__ == "__main__":
+    # call it into a single wrapped function
+    main()
